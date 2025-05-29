621d7e9e552d4f5a861f6ae035546cb94f9a3d13
Daniel <owenjodaniel19@gmail.com>
2025-05-29 14:36:18 +0300

    enviroment variables

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-  },
+  
 };
 
 export default nextConfig;
