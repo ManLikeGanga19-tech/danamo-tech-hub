3f8ef9b781e32074859c28045ccc50e7890e8100
Daniel <owenjodaniel19@gmail.com>
2025-06-06 14:17:48 +0300

    account settings

---
+++ src/app/account/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from "react";
+import UserAddressCard from "@/components/user-profile/UserAddressCard";
+import UserInfoCard from "@/components/user-profile/UserInfoCard";
+import UserMetaCard from "@/components/user-profile/UserMetaCard";
+import { Navbar1 } from "@/components/navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
+export default function Profile() {
+    return (
+        <div>
+            <Navbar1 />
+            <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
+                <h3 className="mb-5 text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-7">
+                    Profile
+                </h3>
+                <div className="space-y-6">
+                    <UserMetaCard />
+                    <UserInfoCard />
+                    <UserAddressCard />
+                </div>
+            </div>
+            <Footer/>
+        </div>
+    );
+}
