53ad3c8f3ad1bf2483821335f4187a0bfa9f459e
Daniel <owenjodaniel19@gmail.com>
2025-06-23 03:41:53 +0300

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import type { Metadata } from "next";
 import { ReactNode } from 'react';
 import { ThemeProvider } from 'next-themes';
 import { SpeedInsights } from "@vercel/speed-insights/next";
@@ -9,6 +9,10 @@ import { Toaster } from '@/components/ui/sonner';
 import WelcomeCard from '@/components/WelcomePopover';
 import './globals.css';
 
+export const metadata: Metadata = {
+  title: "Danamo-Tech",
+  description: "Company head-quartered in Nairobi, Kenya. Founded in 2024, the company was established to bridge the technology gap faced by small business and startups in need of affordable digital solutions.",
+};
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
