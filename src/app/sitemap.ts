b8954a12be086fd5e2263adf13608e99520b76cc
Daniel <owenjodaniel19@gmail.com>
2025-09-29 22:42:17 +0300

    sitemap added

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl = "https://danamo-tech.co.ke";
+
+    return [
+        {
+            url: `${baseUrl}/about`,
+            lastModified: new Date().toISOString(), 
+            changeFrequency: "monthly",
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/services`,
+            lastModified: new Date().toISOString(),
+            changeFrequency: "weekly",
+            priority: 0.9,
+        },
+    ];
+}
